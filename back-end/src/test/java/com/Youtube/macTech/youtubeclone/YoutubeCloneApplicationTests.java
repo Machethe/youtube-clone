f791ef5ff405309332ff3a73375e96db1991d77e
machethe <lebepemachethe@gmail.com>
2022-04-18 21:25:59 +0200

    Initial commit

---
+++ back-end/src/test/java/com/Youtube/macTech/youtubeclone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Youtube.macTech.youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
